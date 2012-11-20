e676c775a25e9bad0ecd377b8dec658bbe187faf
Clemens Park <clemens.park@gmail.com>
2012-11-20 13:46:05 -0500

    makes the registry store the mapped classes as strings, to avoid constant reloading issues

--- lib/edr/registry.rb
+++ lib/edr/registry.rb
@@ -20,23 +20,25 @@ module Edr
       self.instance_eval &block
     end
 
+    # the mapping is done by the class names, to avoid
+    # class-reloading issues in Rails dev mode
     def map model_class, data_class
-      @model_to_data_map[model_class] = data_class
-      @data_to_model_map[data_class] = model_class
+      @model_to_data_map[model_class.to_s] = data_class.to_s
+      @data_to_model_map[data_class.to_s] = model_class.to_s
     end
 
     def data_class_for model_class
-      data_class = @model_to_data_map[model_class]
+      data_class = @model_to_data_map[model_class.to_s]
       return OpenStruct unless data_class
 
-      data_class
+      data_class.constantize
     end
 
     def model_class_for data_class
-      model_class = @data_to_model_map[data_class]
+      model_class = @data_to_model_map[data_class.to_s]
       raise "No model class for #{data_class.to_s}" unless model_class
 
-      model_class
+      model_class.constantize
     end
 
     private
