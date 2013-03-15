160823bc78411206bf37b557d252daf672ede60e
Evan Light <evan@tripledogdare.net>
2013-03-14 21:32:24 -0400

    Modified registry to look up models by convention

--- lib/edr/registry.rb
+++ lib/edr/registry.rb
@@ -3,21 +3,37 @@ require 'ostruct'
 module Edr
   class Registry
     def self.define(&block)
-      @@instance = self.new(&block)
+      @instance = self.new(&block)
     end
 
     def self.data_class_for(model_class)
-      @@instance.data_class_for(model_class)
+      @instance.data_class_for(model_class)
     end
 
     def self.model_class_for(data_class)
-      @@instance.model_class_for(data_class)
+      @instance.model_class_for(data_class)
     end
 
+    def self.map_models_to_mappers
+      define unless @instance
+
+      ObjectSpace.each_object do |o|
+        next unless o.is_a?(Class) &&
+          o.ancestors.include?(ActiveRecord::Base) &&
+          o.to_s =~ /Data$/
+        model_name = o.to_s.sub(/Data$/, "")
+
+        next unless Object.const_defined?(model_name)
+
+        model_class = Object.const_get(model_name)
+        @instance.map(model_class, o)
+      end
+    end
+    
     def initialize(&block)
       @data_to_model_map = {}
       @model_to_data_map = {}
-      self.instance_eval &block
+      #self.instance_eval &block
     end
 
     # the mapping is done by the class names, to avoid
@@ -31,14 +47,12 @@ module Edr
       data_class = @model_to_data_map[model_class.to_s]
       return OpenStruct unless data_class
 
-      data_class.constantize
+      Object.const_get(data_class)
     end
 
     def model_class_for data_class
       model_class = @data_to_model_map[data_class.to_s]
-      raise "No model class for #{data_class.to_s}" unless model_class
-
-      model_class.constantize
+      model_class ? model_class.constantize : nil
     end
 
     private
